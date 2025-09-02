1941c094bbcc94afbf3f5453abf7053bc793b324
Sheyla <sheylachuco123@gmail.com>
2025-09-02 17:51:16 -0500

    agrege mas metodos al CRUD básico

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,8 +1,19 @@
 package pe.edu.tecsup.lab03.controllers;
 
+import java.util.zip.ZipFile;
+
 public class StudentController {
 
     public void index() {
         System.out.println("Controlador de estudiantes funcionando...");
     }
+
+    public int countStudents() {
+        ZipFile students;
+        System.out.println("Cantidad total de estudiantes: " + students.size());
+        return students.size();
+    }
+
+
+
 }
