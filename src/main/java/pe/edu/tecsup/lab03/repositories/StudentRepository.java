4da4854986bd0349c6f6eb85161275827eb41b55
SheylaChuco <sheyla.chuco@tecsup.edu.pe>
2025-09-02 14:38:50 -0500

    Commit agregando metodos a studentRepository

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,8 +1,23 @@
 package pe.edu.tecsup.lab03.repositories;
 
+import pe.edu.tecsup.lab03.entities.StudentEntity;
+
+import javax.xml.crypto.XMLCryptoContext;
+
 public class StudentRepository {
 
     public void save() {
         System.out.println("Repositorio: guardando estudiante...");
+    }
+
+    public void save(StudentEntity student) {
+        XMLCryptoContext storage;
+        storage.put(student.getId(), student);
+        System.out.println("Repositorio: estudiante guardado -> " + student);
+    }
+
+    // Buscar estudiante por id
+    public Optional<StudentEntity> findById(String id) {
+        return Optional.ofNullable(storage.get(id));
     }
 }
